70dfb4106e879805f9e5827c482c033dc1a56cdf
Chunming <chunmingchin@gmail.com>
2012-02-26 12:13:13 -0800

    Change

--- particles/serial.cpp
+++ particles/serial.cpp
@@ -73,16 +73,16 @@ int main( int argc, char **argv )
 
     for (int ndx=0; ndx<n; ndx++) { // For each particle
        xIdx = (particles[ndx].x)/subBlockLen; 
-       printf("xIdx is %d \n", xIdx);
+//       printf("xIdx is %d \n", xIdx);
        yIdx = (particles[ndx].y)/subBlockLen;
-       printf("yIdx is %d \n", yIdx);
+//       printf("yIdx is %d \n", yIdx);
        bdx = 0;
        // Store into first non-NULL index in array
        while (binArray[yIdx+(xIdx*subBlockNum)][bdx]!=NULL) {
-          printf("bdx is %d \n", bdx)
+//          printf("bdx is %d \n", bdx)
           bdx++;
        }
-       printf("Bin index is %d \n", yIdx+xIdx*subBlockNum);
+//       printf("Bin index is %d \n", yIdx+xIdx*subBlockNum);
        binArray[yIdx+(xIdx*subBlockNum)][bdx] = &particles[ndx];
        binParticleNum[yIdx+(xIdx*subBlockNum)]++; // Increment bin count
     }
