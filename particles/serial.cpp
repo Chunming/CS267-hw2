81c69a5f13603e25400a732cd1681c47c4d13148
Chunming <chunmingchin@gmail.com>
2012-02-26 19:20:13 -0800

    Debugger mode

--- particles/serial.cpp
+++ particles/serial.cpp
@@ -70,13 +70,10 @@ int main( int argc, char **argv )
 
     for (int ndx=0; ndx<n; ndx++) { // For each particle
        xIdx = (particles[ndx].x)/subBlockLen; // Takes values 0-4
-//       printf("xIdx is %d \n", xIdx);
        yIdx = (particles[ndx].y)/subBlockLen; // Takes values 0-4
-//       printf("yIdx is %d \n", yIdx);
        bdx = 0;
        // Store into first non-NULL index in array
        while (binArray[yIdx+(xIdx*subBlockNum)][bdx]!=NULL) {
-//          printf("bdx is %d \n", bdx)
           bdx++;
        }
 //       printf("Bin index is %d \n", yIdx+xIdx*subBlockNum);
