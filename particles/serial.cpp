f50c9e30730425beb5f53057c5bd3c213fa2d062
Chunming <chunmingchin@gmail.com>
2012-02-26 19:14:04 -0800

    Deleted global repo files

--- particles/serial.cpp
+++ particles/serial.cpp
@@ -264,7 +264,7 @@ int main( int argc, char **argv )
 	   idxCount += binParticleNum[b];
 	}
 
-        printf("The idxCount is %d \n", idxCount);
+        //printf("The idxCount is %d \n", idxCount);
         
 	
 	//
